57d868ee1eb8cf5e23f0dc4159e30a9f7a1e0b33
reejavijin2002 <vijincp7@gmail.com>
2024-02-15 10:36:49 +0530

    seventeen

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -12,134 +12,209 @@ import img10 from "../assets/image/Setting.png";
 import logo from "../assets/image/mellon_brand._logo_FIN__2__page-0001-removebg-preview.png";
 import { Link } from "react-router-dom";
 import home from "../assets/image/main.jpg";
+import img11 from "../assets/image/down-arrow.png";
 
 const Dashboard = () => {
   const [open, setOpen] = useState(true);
-  const Menus = [
-    { title: "Dashboard", src: img3 },
-    { title: "Inbox", src: img4 },
-    { title: "Accounts", src: img5, gap: true },
-    { title: "Schedule ", src: img6 },
-    { title: "Search", src: img7 },
-    { title: "Analytics", src: img8 },
-    { title: "Files ", src: img9, gap: true },
-    { title: "Setting", src: img10 },
-  ];
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [activeListItem, setActiveListItem] = useState("Dashboard"); // Track active list item
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
 
   return (
     <div className="flex">
-    <div
-      className={`${
-        open ? "w-[20%]" : "w-20 "
-      } bg-green-600 h-auto p-0 firstdiv  pt-0 relative  duration-300 rounded-3xl m-1 `}
-    >
-      <img
-        src={logo}
-        className={`cursor-pointer duration-500  h-16 ${
-          open && "rotate-[360deg]"
-        }`}
-      />
-      <ul className="pt-0">
-        <li className="flex  rounded-md py-2 cursor-pointer hover:bg-light-white text-white font-semibold text-sm items-center gap-x-4 mt-2 bg-light-white">
-          <img src={img3} />
-          <span className={`${!open && "hidden"} origin-left duration-200`}>
-            Dashboard
-          </span>
-        </li>
-        <li className="flex  rounded-md py-2 cursor-pointer hover:bg-light-white text-white font-semibold text-sm items-center gap-x-4 mt-2">
-          <img src={img4} />
-          <span className={`${!open && "hidden"} origin-left duration-200`}>
-            Inbox
-          </span>
-        </li>
-        <li className="flex  rounded-md py-2 cursor-pointer hover:bg-light-white text-white font-semibold text-sm items-center gap-x-4 mt-2 bg-light-white">
-          <img src={img5} />
-          <span className={`${!open && "hidden"} origin-left duration-200`}>
-            Accounts
-          </span>
-        </li>
-        <li className="flex  rounded-md py-2 cursor-pointer hover:bg-light-white text-white font-semibold text-sm items-center gap-x-4 mt-2 bg-light-white">
-          <img src={img6} />
-          <span className={`${!open && "hidden"} origin-left duration-200`}>
-          Schedule
-          </span>
-        </li>
-        <li className="flex  rounded-md py-2 cursor-pointer hover:bg-light-white text-white font-semibold text-sm items-center gap-x-4 mt-2 bg-light-white">
-          <img src={img7} />
-          <span className={`${!open && "hidden"} origin-left duration-200`}>
-          Search
-          </span>
-        </li>
-        <li className="flex  rounded-md py-2 cursor-pointer hover:bg-light-white text-white font-semibold text-sm items-center gap-x-4 mt-2 bg-light-white">
-          <img src={img8} />
-          <span className={`${!open && "hidden"} origin-left duration-200`}>
-          Analytics
-          </span>
-        </li>
-        <li className="flex  rounded-md py-2 cursor-pointer hover:bg-light-white text-white font-semibold text-sm items-center gap-x-4 mt-2 bg-light-white">
-          <img src={img9} />
-          <span className={`${!open && "hidden"} origin-left duration-200`}>
-          Files
-          </span>
-        </li>
-        <li className="flex  rounded-md py-2 cursor-pointer hover:bg-light-white text-white font-semibold text-sm items-center gap-x-4 mt-2 bg-light-white">
-          <img src={img10} />
-          <span className={`${!open && "hidden"} origin-left duration-200`}>
-          Setting
-          </span>
-        </li>
-      </ul>
-      <img
-        src={img1}
-        className={`absolute cursor-pointer -right-3 slidingicon xs:invisible sm:visible top-9 w-7 border-dark-purple
+      <div
+        className={`${
+          open ? "w-[20%]" : "w-20 "
+        } bg-gradient-to-r from-green-500 via-green-500 to-green-600  h-auto p-0 firstdiv  pt-0 relative  duration-300 rounded-3xl m-1 `}
+      >
+        <img
+          src={logo}
+          className={`cursor-pointer duration-500  h-28 ${
+            open && "rotate-[360deg] w-36"
+          }`}
+        />
+        <ul className="pt-0" id="ul1">
+          <li
+            className={`flex  rounded-md px-2 py-2 hover:bg-green-400 transition cursor-pointer hover:bg-light-white text-white font-semibold text-sm items-center gap-x-4 mt-2 bg-light-white ${
+              activeListItem === "Dashboard" ? "activeListItem" : ""
+            }`}
+            onClick={() => setActiveListItem("Dashboard")}
+          >
+            {" "}
+            <img src={img3} />
+            <span className={`${!open && "hidden"} origin-left duration-200`}>
+              Dashboard
+            </span>
+          </li>
+          <li
+            className={`flex  rounded-md px-2 py-2 hover:bg-green-400 transition cursor-pointer hover:bg-light-white text-white font-semibold text-sm items-center gap-x-4 mt-2 bg-light-white ${
+              activeListItem === "inbox" ? "activeListItem" : ""
+            }`}
+            onClick={() => setActiveListItem("inbox")}
+          >
+            {" "}
+            <img src={img4} />
+            <span className={`${!open && "hidden"} origin-left duration-200`}>
+              Inbox
+            </span>
+          </li>
+          <li
+            className={`flex  rounded-md px-2 py-2 hover:bg-green-400 transition cursor-pointer hover:bg-light-white text-white font-semibold text-sm items-center gap-x-4 mt-2 bg-light-white ${
+              activeListItem === "Accounts" ? "activeListItem" : ""
+            }`}
+            onClick={() => setActiveListItem("Accounts")}
+          >
+            {" "}
+            <img src={img5} />
+            <span className={`${!open && "hidden"} origin-left duration-200`}>
+              Accounts
+            </span>
+          </li>
+          <li
+            className="flex  rounded-md px-2 py-2 cursor-pointer hover:bg-green-400 hover:bg-light-white text-white font-semibold text-sm items-center gap-x-4 mt-2 bg-light-white"
+            onClick={toggleDropdown}
+          >
+            <img src={img6} />
+            <span className={`${!open && "hidden"} origin-left duration-200`}>
+              Schedule
+            </span>
+            <img src={img11} className="ml-auto" />
+          </li>
+          {showDropdown && (
+            <div className=" ">
+              <li
+                className={`flex  rounded-md ml-3 px-2 py-2 hover:bg-green-400 transition cursor-pointer hover:bg-light-white text-white font-semibold text-sm items-center gap-x-4 mt-2 bg-light-white ${
+                  activeListItem === "Time" ? "activeListItem" : ""
+                }`}
+                onClick={() => setActiveListItem("Time")}
+              >
+                {" "}
+                <img src={img5} />
+                <span
+                  className={`${!open && "hidden"} origin-left duration-200`}
+                >
+                  Time
+                </span>
+              </li>
+              <li
+                className={`flex  rounded-md ml-3 px-2 py-2 hover:bg-green-400 transition cursor-pointer hover:bg-light-white text-white font-semibold text-sm items-center gap-x-4 mt-2 bg-light-white ${
+                  activeListItem === "Date" ? "activeListItem" : ""
+                }`}
+                onClick={() => setActiveListItem("Date")}
+              >
+                {" "}
+                <img src={img6} />
+                <span
+                  className={`${!open && "hidden"} origin-left duration-200`}
+                >
+                  Date
+                </span>
+              </li>
+            </div>
+          )}
+          <li
+            className={`flex  rounded-md px-2 py-2 hover:bg-green-400 transition cursor-pointer hover:bg-light-white text-white font-semibold text-sm items-center gap-x-4 mt-2 bg-light-white ${
+              activeListItem === "Search" ? "activeListItem" : ""
+            }`}
+            onClick={() => setActiveListItem("Search")}
+          >
+            {" "}
+            <img src={img7} />
+            <span className={`${!open && "hidden"} origin-left duration-200`}>
+              Search
+            </span>
+          </li>
+          <li
+            className={`flex  rounded-md px-2 py-2 hover:bg-green-400 transition cursor-pointer hover:bg-light-white text-white font-semibold text-sm items-center gap-x-4 mt-2 bg-light-white ${
+              activeListItem === "Analytics" ? "activeListItem" : ""
+            }`}
+            onClick={() => setActiveListItem("Analytics")}
+          >
+            {" "}
+            <img src={img8} />
+            <span className={`${!open && "hidden"} origin-left duration-200`}>
+              Analytics
+            </span>
+          </li>
+          <li
+            className={`flex  rounded-md px-2 py-2 hover:bg-green-400 transition cursor-pointer hover:bg-light-white text-white font-semibold text-sm items-center gap-x-4 mt-2 bg-light-white ${
+              activeListItem === "File" ? "activeListItem" : ""
+            }`}
+            onClick={() => setActiveListItem("File")}
+          >
+            {" "}
+            <img src={img9} />
+            <span className={`${!open && "hidden"} origin-left duration-200`}>
+              Files
+            </span>
+          </li>
+          <li
+            className={`flex  rounded-md px-2 py-2 hover:bg-green-400 transition cursor-pointer hover:bg-light-white text-white font-semibold text-sm items-center gap-x-4 mt-2 bg-light-white ${
+              activeListItem === "Settings" ? "activeListItem" : ""
+            }`}
+            onClick={() => setActiveListItem("Settings")}
+          >
+            {" "}
+            <img src={img10} />
+            <span className={`${!open && "hidden"} origin-left duration-200`}>
+              Setting
+            </span>
+          </li>
+        </ul>
+        <img
+          src={img1}
+          className={`absolute cursor-pointer -right-3 slidingicon xs:invisible sm:visible top-9 w-7 border-dark-purple
          border-2 rounded-full  ${!open && "rotate-180"}`}
-        onClick={() => setOpen(!open)}
-      />
-    </div>
-    <div className="h-screen flex-1 p-7 w-[80%] seconddiv xs:invisible sm:visible">
-      <header className="bg-stone-50 sticky   top-0   shadow border-slate-200z-30 ">
-        <div className="px-4 ">
-          <div className="flex items-center justify-between h-16 ">
-            <Link to="/">
-              <div className="flex caret-transparent">
-                <div>
-                  <img src={logo} alt="" width="45px" height="45px" />
-                </div>{" "}
-                <div className="font-bold text-xl lg:text-2xl mt-2  "></div>
-              </div>
-            </Link>
-
-            <div className="flex items-center space-x-3">
-              <div></div>
-              <div className=" xs:hidden sm:hidden md:flex text-sm">
-                <div className="ml-4 mr-2">
-                  <div className="flex  cursor-pointer "></div>
+          onClick={() => setOpen(!open)}
+        />
+      </div>
+      <div className="h-screen flex-1 p-7 w-[80%] seconddiv xs:invisible sm:visible">
+        <header className="bg-stone-50 sticky   top-0   shadow border-slate-200z-30 ">
+          <div className="px-4 ">
+            <div className="flex items-center justify-between h-16 ">
+              <Link to="/">
+                <div className="flex caret-transparent">
+                  <div>
+                    <img src={logo} alt="" width="100px" height="45px" />
+                  </div>{" "}
+                  <div className="font-bold text-xl lg:text-2xl mt-2  "></div>
                 </div>
-                <Link to="">
-                  <img
-                    className="rounded-full  border border-fuchsia-950 "
-                    style={{
-                      border: "0.2rem solid fuchsia",
-                      width: "40px",
-                      height: "40px",
-                      transition: "border-color 0.3s ease", // Adding transition for a smoother effect
-                    }}
-                    src={home}
-                    onMouseOver={(e) => {
-                      e.target.style.borderColor = "transparent"; // Change border color to transparent on hover
-                    }}
-                    onMouseOut={(e) => {
-                      e.target.style.borderColor = "fuchsia"; // Change border color back to the original color on mouse out
-                    }}
-                  />
-                </Link>
+              </Link>
+
+              <div className="flex items-center space-x-3">
+                <div></div>
+                <div className=" xs:hidden sm:hidden md:flex text-sm">
+                  <div className="ml-4 mr-2">
+                    <div className="flex  cursor-pointer "></div>
+                  </div>
+                  <Link to="">
+                    <img
+                      className="rounded-full  border border-fuchsia-950 "
+                      style={{
+                        border: "0.2rem solid fuchsia",
+                        width: "40px",
+                        height: "40px",
+                        transition: "border-color 0.3s ease", // Adding transition for a smoother effect
+                      }}
+                      src={home}
+                      onMouseOver={(e) => {
+                        e.target.style.borderColor = "transparent"; // Change border color to transparent on hover
+                      }}
+                      onMouseOut={(e) => {
+                        e.target.style.borderColor = "fuchsia"; // Change border color back to the original color on mouse out
+                      }}
+                    />
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </header>
+        </header>
+      </div>
     </div>
-  </div>
   );
 };
 export default Dashboard;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,33 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import img from '../assets/image/Login-amico.png'
+import { Field, Form, Formik } from "formik";
 
 
 const Login = () => {
+  const [username,setUsername]=useState("")
+  const [password,setPassword]=useState("")
+
   const [credentials, setCredentials] = useState({
     username: "",
     password: "",
   });
-
+  const initialvalues1 = {
+    username: "",
+    password:""
+   
+  };
+  const validateForm = () => {
+    const errors = {};
+    if (!username.trim()) {
+      errors.username = "Username is required";
+    }
+    if (!password.trim()) {
+      errors.password = "Password is required";
+    }
+  
+    return errors;
+  };
   const navigate = useNavigate();
 
   const handleInputChange = (e) => {
@@ -20,10 +39,8 @@ const Login = () => {
   };
 
   const handleLogin = (e) => {
-    e.preventDefault();
-
- 
-      navigate("/dashboard");
+  
+        navigate("/dashboard");
   
   };
 
@@ -39,7 +56,9 @@ const Login = () => {
             <h2 className="text-2xl font-semibold  mb-6">
               Login
             </h2>
-            <form>
+            <Formik initialValues={{initialvalues1}} validate={validateForm}>
+            {(formik)=>(
+            <Form>
               <div className="mb-4 ">
                 <label
                   htmlFor="username"
@@ -47,15 +66,18 @@ const Login = () => {
                 >
                   Username:
                 </label>
-                <input
+                <Field
                   type="text"
                   id="username"
                   name="username"
                   className="w-full border p-2 rounded-md"
                   placeholder="Enter your username"
-                  onChange={handleInputChange}
-                  value={credentials.username}
+                  onChange={(e)=>setUsername(e.target.value)}
+                  value={username}
                 />
+                  {!username ? (
+                  <div className="validate">{formik.errors.username}</div>
+                ) : null}
               </div>
               <div className="mb-6 ">
                 <label
@@ -64,24 +86,29 @@ const Login = () => {
                 >
                   Password:
                 </label>
-                <input
+                <Field
                   type="password"
                   id="password"
                   name="password"
                   className="w-full border p-2 rounded-md"
                   placeholder="Enter your password"
-                  onChange={handleInputChange}
-                  value={credentials.password}
+                  onChange={(e)=>setPassword(e.target.value)}
+                  value={password}
                 />
+                  {!password ? (
+                  <div className="validate">{formik.errors.password}</div>
+                ) : null}
               </div>
               <button
                 type="submit"
                 className="w-full bg-green-500   hover:bg-white hover:outline hover:text-green-500    p-2 rounded-md "
-                onClick={handleLogin}
+                onClick={() => formik.handleSubmit(username && password ?handleLogin():null)}
               >
                 Login
               </button>
-            </form>
+            </Form>
+            )}
+            </Formik>
             <div className="mt-4 text-center  ">
               <p className="text-sm">
                 Don't have an account?{" "}
